5cdfa26da12c07d5672bcd4b99e06ba7efdaa958
maximt <vbmaxim@gmail.com>
2021-06-27 17:52:44 +0700

    unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,25 @@
 from selenium import webdriver
+import unittest
 
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-try:
-    browser.get('http://localhost:8000')
-    assert 'The install worked successfully! Congratulations!' in browser.title
-finally:
-    browser.close()
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_start_and_retrieve_list(self):
+        self.browser.get('http://localhost:8000')
+
+        # is this main page?
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('End test')
+
+        #todo: start list
+
+
+if __name__ == '__main__':
+    unittest.main()
