96dce807faefa507adcc7c61606c16cef698e910
maximt <vbmaxim@gmail.com>
2021-06-28 17:39:46 +0700

    unittest home page

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,11 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
 
-    def test_bad(self):
-        self.assertEqual(1+1, 3)
+    def test_root_url_to_home_page(self):
+        view = resolve('/')
+        self.assertEqual(view.func, home_page)
 
-        
