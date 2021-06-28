e88cb5540164f616ddc3b7dece19cee62f58614d
maximt <vbmaxim@gmail.com>
2021-06-28 20:05:24 +0700

    test create item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -16,9 +18,30 @@ class NewVisitorTest(unittest.TestCase):
 
         # is this main page?
         self.assertIn('To-Do', self.browser.title)
-        self.fail('End test')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
-        #todo: start list
+        # create new item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 
+            'Enter item'
+        )
+        inputbox.send_keys('Buy feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        time.sleep(1)
+
+        # check is item created
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1' for row in rows)
+        )
+
+        #todo create another item
+
+        self.fail('End test')
 
 
 if __name__ == '__main__':
