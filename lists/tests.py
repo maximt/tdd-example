65624100f25eccae5b987c8d0e6958a4bb351310
maximt <vbmaxim@gmail.com>
2021-06-29 16:45:19 +0700

    test home page template

--- lists/tests.py
+++ lists/tests.py
@@ -7,18 +7,8 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_url_to_home_page(self):
-        view = resolve('/')
-        self.assertEqual(view.func, home_page)
-
-    def test_home_page_correct_html(self):
+    def test_home_page_template(self):
         response = self.client.get('/')
-
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
         self.assertTemplateUsed(response, 'home.html')
 
         
