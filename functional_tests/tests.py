c7746b4e5301375cf0d4dacaaeb603f7e05f9e3a
maximt <vbmaxim@gmail.com>
2021-07-06 18:52:13 +0700

    add template styling test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -121,3 +121,13 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_element('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10)
