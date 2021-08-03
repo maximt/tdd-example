288634b2bf216a83dd4b0cfa17dfc5744a832ce5
maximt <vbmaxim@gmail.com>
2021-08-03 19:02:55 +0700

    add form to views and templates, tests are broken

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from .models import Item, List
+from .forms import ItemForm
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'form': ItemForm()})
+
 
 def page_list(request, list_id):
     list_ = List.objects.get(id=list_id)
