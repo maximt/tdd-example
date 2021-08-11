3d2faea45c343e81c463c67b20b0bee099276b50
maximt <vbmaxim@gmail.com>
2021-08-11 20:00:15 +0700

    mock for send_mail

--- superlists/settings.py
+++ superlists/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lists',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATIC_ROOT = Path.absolute(Path.joinpath(BASE_DIR, '../static/'))
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
--- superlists/urls.py
+++ superlists/urls.py
@@ -17,11 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from lists import views as lists_views
 from lists import urls as lists_urls
-
+from accounts import urls as accounts_urls
 
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', lists_views.home_page, name='home'),
-    path('lists/', include(lists_urls))
+    path('lists/', include(lists_urls)),
+    path('accounts/', include(accounts_urls)),
 ]
