4cbb33369b9ce6d4c6ceff3976dec8b7641a890d
maximt <vbmaxim@gmail.com>
2021-08-13 16:44:55 +0700

    logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from accounts import views
 
 
 urlpatterns = [
     path('send_login_email', views.send_login_email, name='send_login_email'),
     path('login', views.login, name='login'),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
