eaf6d762682f8d00f094d0ce131b7a089077afd1
maximt <vbmaxim@gmail.com>
2021-08-17 16:11:15 +0700

    mail test

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,4 +1,7 @@
 import re
+import time
+import os
+import  poplib
 from selenium.webdriver.common.keys import Keys
 from django.core import mail
 from .base import FunctionalTest
@@ -9,6 +12,38 @@ SUBJECT = 'Your login link for Superlists'
 
 
 class LoginTest(FunctionalTest):
+
+    def wait_for_email(self, test_email, subject):
+        if not self.staging_server:
+            email = mail.outbox[0]
+            self.assertIn(test_email, email.to)
+            self.assertEqual(email.subject, subject)
+            return email.body
+
+        email_id = None
+        start = time.time()
+        inbox = poplib.POP3_SSL('pop3.promsvyaz.pro')
+        try:
+            inbox.user(test_email)
+            inbox.pass_(os.environ['EMAIL_PASSWORD'])
+
+            while (time.time() - start) < 60:
+                count, _ = inbox.stat()
+                for i in reversed(range(max(1, count - 10), count + 1)):
+                    print('Getting msg', i)
+                    _, lines, __ = inbox.retr(i)
+                    lines = [l.decode('utf8') for l in lines]
+                    print(lines)
+                    if f'Subject: {subject}':
+                        email_id = i
+                        body = '\n'.join(lines)
+                        return body
+
+        finally:
+            if email_id:
+                inbox.dele(email_id)
+            inbox.quit()
+        
 
     def test_can_get_email_link_to_login(self):
         self.browser.get(self.live_server_url)
